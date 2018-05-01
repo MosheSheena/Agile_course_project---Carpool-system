c94f062aaa56007905ebbc9a793894dfdccf9c92
amitlevy21 <amitlevy347@gmail.com>
2018-05-01 19:09:56 +0300

    added attributes for classes

--- src/Commuter.java
+++ src/Commuter.java
@@ -1,18 +1,47 @@
+import java.util.ArrayList;
 
 public class Commuter extends Person{
 	
 	private int numOfRides;
 	private int numOfCars;
-	private Car[] carsOwned;
-	private Ride[] historyRides;
+	private ArrayList<Car> carsOwned;
+	private ArrayList<Ride> historyRides;
 	
-	public Commuter(int numOfCars, Car[] carsOwned, String name, String address, int age, String city, int numOfRides) {
+	public Commuter(int numOfCars, ArrayList<Car> carsOwned, String name, String address, int age, String city, int numOfRides) {
 		super(name, address, age, city);
 		this.numOfRides = numOfRides;
 		this.numOfCars = numOfCars;
 		this.carsOwned = carsOwned;
 		
 		this.numOfRides = 0;
+		this.historyRides = new ArrayList<>();
 	}
+
+	public int getNumOfRides() {
+		return numOfRides;
+	}
+
+	public void setNumOfRides(int numOfRides) {
+		this.numOfRides = numOfRides;
+	}
+
+	
+	public ArrayList<Car> getCarsOwned() {
+		return carsOwned;
+	}
+
+	public void setCarsOwned(ArrayList<Car> carsOwned) {
+		this.carsOwned = carsOwned;
+	}
+
+	public int getNumOfCars() {
+		return numOfCars;
+	}
+
+	public ArrayList<Ride> getHistoryRides() {
+		return historyRides;
+	}
+	
+	
 
 }
--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Ride {
 	
@@ -10,6 +11,75 @@ public class Ride {
 	private int pricePerHitchhiker;
 	private Car theCar;
 	private CarOwner carOwner;
-	private Hitchhiker[] hitchhikers;
+	private ArrayList<Hitchhiker> hitchhikers;
+	private int numOfActualHichhikers;
+	private int moneySavedFromRide;
+	
+	public Ride(String destination, String source, Car theCar, CarOwner carOwner, ArrayList<Hitchhiker> hitchhikers,
+			int numOfActualHichhikers) {
+		this.destination = destination;
+		this.source = source;
+		this.theCar = theCar;
+		this.carOwner = carOwner;
+		this.hitchhikers = hitchhikers;
+		this.numOfActualHichhikers = numOfActualHichhikers;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public Car getTheCar() {
+		return theCar;
+	}
+
+	public void setTheCar(Car theCar) {
+		this.theCar = theCar;
+	}
+
+	public CarOwner getCarOwner() {
+		return carOwner;
+	}
+
+	public void setCarOwner(CarOwner carOwner) {
+		this.carOwner = carOwner;
+	}
+
+	public ArrayList<Hitchhiker> getHitchhikers() {
+		return hitchhikers;
+	}
+
+	public void setHitchhikers(ArrayList<Hitchhiker> hitchhikers) {
+		this.hitchhikers = hitchhikers;
+	}
+
+	public int getNumOfActualHichhikers() {
+		return numOfActualHichhikers;
+	}
+
+	public void setNumOfActualHichhikers(int numOfActualHichhikers) {
+		this.numOfActualHichhikers = numOfActualHichhikers;
+	}
+
+	public int getPricePerHitchhiker() {
+		return pricePerHitchhiker;
+	}
+
+	public int getMoneySavedFromRide() {
+		return moneySavedFromRide;
+	}
+	
 	
 }
