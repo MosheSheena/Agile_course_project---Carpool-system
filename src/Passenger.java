1a9479241d5d4f96ca39a89e222577627ac3bce4
moshesheena <moshesheena@gmail.com>
2018-05-02 09:05:21 +0300

    Driver side: Added a few classes.

---
+++ src/Passenger.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+import java.util.Objects;
+
+public class Passenger extends Person{
+
+    private Location source;
+    private Location destination;
+
+    public Passenger(String name, Date birthDate, boolean hasDriverLicense, Location source, Location destination) {
+        super(name, birthDate, hasDriverLicense);
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public Location getSource() {
+        return source;
+    }
+
+    public void setSource(Location source) {
+        this.source = source;
+    }
+
+    public Location getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Location destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "source=" + source +
+                ", destination=" + destination +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Passenger)) return false;
+        if (!super.equals(o)) return false;
+        Passenger passenger = (Passenger) o;
+        return Objects.equals(getSource(), passenger.getSource()) &&
+                Objects.equals(getDestination(), passenger.getDestination());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), getSource(), getDestination());
+    }
+}
---
+++ src/Ride.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Ride {
+
+    private Integer identifier;
+    private Driver assignedDriver;
+    private List<Passenger> passengers;
+    private List<Location> routes;
+}
+
